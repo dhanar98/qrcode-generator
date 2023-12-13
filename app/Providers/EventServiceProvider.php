da082dc5f1b49b9b0f8b8cfd854014f15c4c6df8
dhanar98 <dhanasekarravi98@gmail.com>
2023-12-14 00:42:14 +0530

    obeservers added in eventservice provider for booting

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,7 +2,20 @@
 
 namespace App\Providers;
 
+use App\Models\QrCode;
+use App\Models\QrCodeConfig;
+use App\Models\QrCodeEmail;
+use App\Models\QrCodeGeo;
+use App\Models\QrCodePhoneNumber;
+use App\Models\QrCodeSms;
+use App\Models\QrCodeWifi;
 use App\Models\User;
+use App\Observers\QrCodeConfigObserver;
+use App\Observers\QrCodeEmailObserver;
+use App\Observers\QrCodeGeoObserver;
+use App\Observers\QrCodeObserver;
+use App\Observers\QrCodeSmsObserver;
+use App\Observers\QrCodeWifiObserver;
 use App\Observers\UserObserver;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
@@ -24,6 +37,13 @@ class EventServiceProvider extends ServiceProvider
 
     protected $observers = [
         User::class => UserObserver::class,
+        QrCode::class => QrCodeObserver::class,
+        QrCodeConfig::class => QrCodeConfigObserver::class,
+        QrCodeEmail::class => QrCodeEmailObserver::class,
+        QrCodeGeo::class => QrCodeGeoObserver::class,
+        QrCodePhoneNumber::class => QrCodePhoneNumber::class,
+        QrCodeSms::class => QrCodeSmsObserver::class,
+        QrCodeWifi::class => QrCodeWifiObserver::class
     ];
 
     /**
